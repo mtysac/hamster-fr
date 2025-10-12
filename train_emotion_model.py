f04de1adcd6f2fecc01133d7e6648ca62a278385
mtysac <mtysabelcab@gmail.com>
2025-10-12 12:38:08 -0230

    Updated Output

--- train_emotion_model.py
+++ train_emotion_model.py
@@ -1,8 +1,7 @@
 import tensorflow as tf
-# pip install scipy if tensorflow.keras not working
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.preprocessing.image import ImageDataGenerator # type: ignore
+from tensorflow.keras.models import Sequential # type: ignore
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout # type: ignore
 import os
 
 TRAIN_DIR = "dataset/train"
